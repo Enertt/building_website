480c61ebf5ca2d7766b8b10cc7c920ec1b1a0b9b
y_zavorotynskyi <ezavor111@gmail.com>
2023-09-09 16:02:13 +0200

    face of company V1

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -5,8 +5,8 @@ const Nav = () => {
     return(
         <div className={styles.navWrapper}>
             <div className={styles.navElement}><NavLink to='/aboutCompany'>ПРО КОМПАНIЮ</NavLink></div>
-            <div className={styles.navElement}><NavLink to='/faceOfCompany'>НАШI МОЖЛИВОСТI</NavLink></div>
-            <div className={styles.navElement}><NavLink to='/realizedProjects'>ОБЛИЧЧЯ КОМПАНIЇ</NavLink></div>
+            <div className={styles.navElement}><NavLink to='/realizedProjects'>НАШI ПРОЕКТИ</NavLink></div>
+            <div className={styles.navElement}><NavLink to='/faceOfCompany'>ОБЛИЧЧЯ КОМПАНIЇ</NavLink></div>
             {/* <div className={styles.navElement}><NavLink to='/currentlyBilding'>Об‘єкти які ми зараз будуємо</NavLink></div>
             <div className={styles.navElement}><NavLink to='/rebilding'>Об‘єкти які відбудовуємо</NavLink></div> */}
             <div className={styles.navElement}><NavLink to='/contacts'>КОНТАКТИ</NavLink></div>
