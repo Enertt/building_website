2c937c3e5895979a0699c00996bc80b75683e9fb
y_zavorotynskyi <ezavor111@gmail.com>
2023-09-11 20:45:29 +0200

    before deploying v1

--- src/index.js
+++ src/index.js
@@ -6,15 +6,20 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <App /*appState={state} dispatch={dispatch}*/ />
-      </Provider>
-    </BrowserRouter>
+    {/* <HashRouter> */}
+    {/* BrowserRouter - for developing and hosting*/}
+    {/* HashRouter - for GH pages ONLY*/}
+      <BrowserRouter>
+        <Provider store={store}>
+          <App /*appState={state} dispatch={dispatch}*/ />
+        </Provider>
+      </BrowserRouter>
+    {/* </HashRouter> */}
   </React.StrictMode>
 );
 
