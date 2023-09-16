fc158658edfb3048581d7b9f5055178e586ed3c3
y_zavorotynskyi <ezavor111@gmail.com>
2023-09-16 22:01:33 +0200

    admin page v2

---
+++ src/Components/Admin/AddNewForm.js
@@ -0,0 +1,11 @@
+import styles from "./addNew.module.css"
+
+const AddNewForm = () => {
+    return(
+        <div className={styles.wrapper}>
+            
+        </div>
+    )
+}
+
+export default AddNewForm
--- src/Components/Admin/AdminPage.js
+++ src/Components/Admin/AdminPage.js
@@ -1,10 +1,12 @@
 import styles from './adminPage.module.css'
 import dataJson from '../pages/News/news.json'
+import AddNewForm from './AddNewForm'
 import { useNavigate } from 'react-router-dom'
+import { useState } from 'react'
 import { useEffect } from 'react'
 
 const AdminPage = (props) => {
-
+    const [addNewFormState, setAddNewFormState] = useState(false)
     const navigate = useNavigate()
 
     useEffect(() => {
@@ -36,7 +38,7 @@ const AdminPage = (props) => {
                             </div>
                             
                         </div>
-                    <button className={styles.addNewButton}>Додати новину</button>
+                    <button onClick={() => {setAddNewFormState(true)}} className={styles.addNewButton}>Додати новину</button>
                 </div>
                 
 
@@ -57,6 +59,18 @@ const AdminPage = (props) => {
                                 </div>
                             </div>
                         </div>
+                    </div>
+                </div>
+
+
+
+
+                <div className={addNewFormState ? styles.addNewWrapper : styles.addNewWrapperNone}>
+                    <div className={styles.addNewContent}>
+                        <svg onClick={() => {setAddNewFormState(false)}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+                            <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8 2.146 2.854Z" />
+                        </svg>
+                        <AddNewForm/>
                     </div>
                 </div>
             </div>
