b7750cfae10259bbf707b951aca3e75bc350639b
y_zavorotynskyi <ezavor111@gmail.com>
2023-09-19 18:36:02 +0200

    app responsivity

--- src/Components/Menu/Menu.js
+++ src/Components/Menu/Menu.js
@@ -8,7 +8,7 @@ const Menu = (props) => {
     return (
         <div>
             <div>
-                <svg onBlur={() => {menuState(!menu)}} onClick={() => {menuState(!menu)}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+                <svg className={props.color === 'white' ? styles.svg : styles.svgB} onBlur={() => {menuState(!menu)}} onClick={() => {menuState(!menu)}} xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z" />
                 </svg>
             </div>
@@ -22,7 +22,7 @@ const Menu = (props) => {
                                 : `${styles.navWrapper} ${styles.hero_navlinkColor}`
                         }
                     >
-                        <ul className={styles.navList}>
+                        <ul className={props.color === 'white' ? styles.navList : styles.navListB}>
                             <li className={`${styles.navItem} ${styles.uppercase_text}`}>
                                 <NavLink to="/news">Новини</NavLink>
                             </li>
